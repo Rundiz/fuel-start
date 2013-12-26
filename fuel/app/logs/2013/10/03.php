7292e28e4edc59d9195b962b5b43458c53f2e69d
vee w, <kawewong@gmail.com>
2013-12-26 08:30:46 +0700

    delete log

--- fuel/app/logs/2013/10/03.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2013-10-03 15:08:02 --> Fatal Error - Call to undefined function Debug::dump() in /data/www/fuelphp/1.7/develop/fuel/core/base.php on line 462
