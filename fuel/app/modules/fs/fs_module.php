a68d9132e12fe68cf2a8b05b3d80bb6e07da0a66
vee w, <kawewong@gmail.com>
2014-03-06 17:47:45 +0700

    add description to updater module.

--- fuel/app/modules/fs/fs_module.php
+++ fuel/app/modules/fs/fs_module.php
@@ -3,7 +3,7 @@
  * Module Name: Fuel Start updater module
  * Module URL:
  * Version:
- * Description:
+ * Description: To run updater you have to browse to fs/admin/update. example: http://localhost/fs/admin/update
  * Author: Vee W.
  * Author URL: http://okvee.net
  */
