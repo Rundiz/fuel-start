aa3018f8e51912a6f2cc3cc9f0a052e4902a2804
vee w, <kawewong@gmail.com>
2014-03-11 06:08:01 +0700

    add error 403

--- fuel/app/classes/controller/error.php
+++ fuel/app/classes/controller/error.php
@@ -10,6 +10,21 @@ class Controller_Error extends \Controller
 {
 
 
+    public function action_403()
+    {
+        Lang::load('error', 'error');
+
+        $output['error_head'] = Lang::get('error.403_error_head');
+        $output['error_content'] = Lang::get('error.403_error_content', array('home_link' => Uri::base()));
+
+        // <head> output ----------------------------------------------------------------------------------------------
+        $output['page_title'] = Lang::get('error.403_page_title');
+        // <head> output ----------------------------------------------------------------------------------------------
+
+        return Response::forge(Theme::instance()->view('error/403_v', $output)->auto_filter(false), 403);
+    }// action_403
+
+
     public function action_404()
     {
         Lang::load('error', 'error');
