df13ea8c87f33d84eb2efcbe8a226a8a0a14ad94
vee w, <kawewong@gmail.com>
2014-04-26 05:10:47 +0700

    change floating table header javascript
    change from custom floating header javascript that has bugs on small
    device to Jquery.floatThead.
    Jquery.floatThead is the perfect floating table header javascript.
    see more at: http://mkoryak.github.io/floatThead/

--- public/public/themes/system/admin/inc_html_foot.php
+++ public/public/themes/system/admin/inc_html_foot.php
@@ -4,6 +4,7 @@
 		<?php echo $theme->asset->js('smartmenus/jquery.smartmenus.min.js'); ?>
 		<?php echo $theme->asset->js('chosen/chosen.jquery.min.js'); ?>
 		<?php echo $theme->asset->js('icheck/icheck.min.js'); ?>
+		<?php echo $theme->asset->js('jquery.floatThead.min.js'); ?>
 		<?php echo $theme->asset->render('fuelstart_footer'); ?> 
 		
 		<?php echo $theme->asset->js('main.js'); ?>
--- public/public/themes/system/assets/js/admin.js
+++ public/public/themes/system/assets/js/admin.js
@@ -43,49 +43,10 @@ function makeBs3Tabs() {
  * @returns {undefined}
  */
 function tableWithFloatingheader() {
-	function UpdateTableHeaders() {
-		$("div.divTableWithFloatingHeader").each(function() {
-			var originalHeaderRow = $(".tableFloatingHeaderOriginal", this);
-			var floatingHeaderRow = $(".tableFloatingHeader", this);
-			var offset = $(this).offset();
-			var scrollTop = $(window).scrollTop();
-			if ((scrollTop > offset.top) && (scrollTop < offset.top + $(this).height())) {
-				floatingHeaderRow.css("visibility", "visible");
-				floatingHeaderRow.css("top", Math.min(scrollTop - offset.top, $(this).height() - floatingHeaderRow.height()) + "px");
-
-				// Copy cell widths from original header
-				$("th", floatingHeaderRow).each(function(index) {
-					var cellWidth = $("th", originalHeaderRow).eq(index).css('width');
-					$(this).css('width', cellWidth);
-				});
-
-				// Copy row width from whole table
-				floatingHeaderRow.css("width", $(this).css("width"));
-			}
-			else {
-				floatingHeaderRow.css("visibility", "hidden");
-				floatingHeaderRow.css("top", "0px");
-			}
-		});
-	}// UpdateTableHeaders
-	
-	// floating header
-	$("table.tableWithFloatingHeader").each(function() {
-		$(this).wrap("<div class=\"divTableWithFloatingHeader\" style=\"position:relative\"></div>");
-		var originalHeaderRow = $("tr:first", this);
-		originalHeaderRow.before(originalHeaderRow.clone());
-		var clonedHeaderRow = $("tr:first", this);
-		clonedHeaderRow.addClass("tableFloatingHeader");
-		clonedHeaderRow.css("position", "absolute");
-		clonedHeaderRow.css("top", "0px");
-		clonedHeaderRow.css("left", $(this).css("margin-left"));
-		clonedHeaderRow.css("visibility", "hidden");
-		originalHeaderRow.addClass("tableFloatingHeaderOriginal");
+	// the table with floating header use this awesome script: http://mkoryak.github.io/floatThead/
+	$("table.tableWithFloatingHeader").floatThead({
+		zIndex: 1
 	});
-	UpdateTableHeaders();
-	$(window).scroll(UpdateTableHeaders);
-	$(window).resize(UpdateTableHeaders);
-	// end floating header
 }// tableWithFloatingheader
 
 
